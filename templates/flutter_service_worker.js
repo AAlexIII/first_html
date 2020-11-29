993b1a3dbcac8d6d8f7acdaf74b027ec3285dc71
AAlexIII <55252571+AAlexIII@users.noreply.github.com>
2020-11-29 20:40:04 +0300

    heroku

--- templates/flutter_service_worker.js
+++ templates/flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f739047ea4243ce6a8666c2db10f35bc",
-"/": "f739047ea4243ce6a8666c2db10f35bc",
+"index.html": "017838de6226c1ba4bc23f654c13faf5",
+"/": "017838de6226c1ba4bc23f654c13faf5",
 "main.dart.js": "433a444a25ecc6f11fa4b124f1af7e9b",
 "manifest.json": "4eeb688371f278e534c35ada125b8445",
 "version.json": "341c8c980ba2204a37ee116f3e4489a4"
